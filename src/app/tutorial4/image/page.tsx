b2e743a1d37b90edaae40b88bfb13810532d179b
omChotaliya2002 <omchotaliya2005@gmail.com>
2025-04-30 11:16:52 +0530

    make some changes.

--- src/app/tutorial4/image/page.tsx
+++ src/app/tutorial4/image/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image, { StaticImageData } from "next/image";
-import img from "/tutorials/next-tutorial/public/img.jpg"; 
 
 const page = () => {
 
@@ -9,7 +8,7 @@ const page = () => {
 
 <>
     <div className='mb-[100px] flex flex-col items-center justify-center mt-[50px]'>
-        <Image src={img} alt='image' height={500} width={500} className='mb-[20px] ring-2 ring-white hover:rounded-4xl hover:cursor-pointer transition-all delay-150'/>
+        <Image src={"/img.jpg"} alt='image' height={500} width={500} className='mb-[20px] ring-2 ring-white hover:rounded-4xl hover:cursor-pointer transition-all delay-150'/>
 
       <details className='flex flex-col items-center justify-center select-none'>
           <summary className='font-semibold text-amber-200 underline underline-offset-[5px]'> Image Optimization </summary>
